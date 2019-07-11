b784c8e06ceb9e8af2745d5ab3fbc24e55579201
yxj <yaoxijun@bjfullchance.com>
2019-07-11 21:12:23 +0800

    test

--- src/main/java/com/example/demo/DemoApplication1.java
+++ src/main/java/com/example/demo/DemoApplication1.java
@@ -9,6 +9,10 @@ public class DemoApplication1 {
 	public static void main(String[] args) {
 		System.out.printf("1");
 		System.out.printf("2");
+		System.out.printf("3");
 		SpringApplication.run(DemoApplication1.class, args);
+		System.out.printf("4");
+		System.out.printf("5");
+		System.out.printf("6");
 	}
 }
