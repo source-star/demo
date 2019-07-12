c697718f43b351cc0235745f35bca7bfc3523542
yxj <yaoxijun@bjfullchance.com>
2019-07-12 21:53:36 +0800

    test

--- src/main/java/com/example/demo/DemoApplication1.java
+++ src/main/java/com/example/demo/DemoApplication1.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication1 {
 
 	public static void main(String[] args) {
-		System.out.printf("Hello World");
+		System.out.printf("Hello World22222222222222222");
 		SpringApplication.run(DemoApplication1.class, args);
 	}
 }
